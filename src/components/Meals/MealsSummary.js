c4347ef45939ae560b9a420f1a0268e6fa059f21
sandeep kumar <Sandeepgyantek01@gmail.com>
2021-08-31 13:30:58 +0530

    added new line mealsummay.js

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -7,10 +7,11 @@ const MealsSummary = () => {
       <p>
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
+        there is addded new line
       </p>
       <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+        All our meals are cooked with high-quality ingredients, just-in-time 
+        this on add new here...
       </p>
     </section>
   );
